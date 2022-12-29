2e719e91681a0910ad6f354407064f695b1ddedc
Daniel Bishop <dannyjb164@gmail.com>
2022-12-29 18:13:47 +1300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+type item = {
+  title: string;
+  to: string;
+  subtitle?: string;
+};
+const items: item[] = [
+  { title: "Home", to: "/", subtitle: "Welcome to the home page" },
+  { title: "About", to: "/about" },
+];
+</script>
+
+<template>
+  <main>
+    <v-list lines="one">
+      <v-list-item
+        v-for="item in items"
+        :key="item.title"
+        :title="item.title"
+        :to="item.to"
+        :subtitle="item.subtitle"
+        link
+      ></v-list-item>
+    </v-list>
+    <!-- <v-icon icon="mdi-github" ></v-icon> -->
+    
+  </main>
+</template>
