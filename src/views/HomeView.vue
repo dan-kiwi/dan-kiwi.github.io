734394005bafe99769acbc5ec8548d8de9e0ac40
dannyjb164 <dannyjb164@gmail.com>
2023-08-18 11:10:29 +1200

    Fixed spelling mistakes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,30 +2,25 @@
   <main>
     <h1>Welcome to my personal website</h1>
     <p>
-      Hello and welcome to my website! I'm Daniel Bishop, currently a student at
-      the University of Canterbury in Christchurch, New Zealand. I'm studying
-      towards a Bachelor of Science degree with a major in Computer Science and
-      a minor in Economics.
+      Hello and welcome to my website! I'm Daniel Bishop, currently a student at the University of Canterbury in
+      Christchurch, New Zealand. I'm studying towards a Bachelor of Science degree with a major in Computer Science and a
+      minor in Economics.
     </p>
     <p>
-      I'm passionate about technology and enjoy learning about new and emerging
-      technologies. I'm particularly interested in web development, and I'm
-      currently working on a number of projects to improve my skills in this
-      area. I'm also interested in artifical intelligence and machine learning,
-      and I'm looking forward to learning more about these fields in the future.
+      I'm passionate about technology and enjoy learning about new and emerging technologies. I'm particularly interested
+      in web development, and I'm currently working on several projects to improve my skills in this area. I'm also
+      interested in artificial intelligence and machine learning, and I'm looking forward to learning more about these
+      fields in the future.
     </p>
     <p>
-      When I'm not coding, you can find me in the gym, playing squash, or
-      planning my next adventure. Speaking of which, I have recently return from
-      Europe as part of a university exchange program with the University of
-      Nottingham. It was an incredible experience, with the opportunity to meet
-      people from all over the world while studying and exploring new places.
+      When I'm not coding, you can find me in the gym, playing squash, or planning my next adventure. Speaking of which, I
+      have recently returned from Europe as part of a university exchange program with the University of Nottingham. It
+      was an incredible experience, with the opportunity to meet people from all over the world while studying and
+      exploring new places.
     </p>
     <p>
-      This website serves as a platform for me to showcase my work and share my
-      projects, CV, and anything else that I find relevant. I'm continually
-      updating and improving it, so make sure to check back regularly for
-      updates.
+      This website serves as a platform for me to showcase my work and share my projects, CV, and anything else that I
+      find relevant. I'm continually updating and improving it, so make sure to check back regularly for updates.
     </p>
     <p>Thanks for stopping by, and I hope you enjoy exploring my website!</p>
   </main>
@@ -38,5 +33,4 @@ main {
   line-height: 1.5;
   max-height: 100vh;
   gap: 1rem;
-}
-</style>
+}</style>
