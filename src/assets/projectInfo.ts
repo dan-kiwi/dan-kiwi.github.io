ea7fa4a91a7039707d0e1d3592a2c59ef1e3dd3c
Daniel Bishop <career@dan-kiwi.dev>
2023-10-02 14:38:45 +1300

    Update pasturemarket subtitle

--- src/assets/projectInfo.ts
+++ src/assets/projectInfo.ts
@@ -73,7 +73,7 @@ export const onTheFarm: ProjectProps = {
 export const pastureMarket: ProjectProps = {
   title: "Pasture Market",
   subtitle:
-    "Marketplace for farmers to trade (WIP) - UC Enterprise Challenge - Startup",
+    "Marketplace for farmers to view prices (WIP) - UC Enterprise Challenge - Startup",
   text: [
     "At the moment, the website serves to display the current market price of farm commodities. However, it's still under development, with numerous bugs and does not accurately reflect current market prices.",
     "Initially, this project began as a Minimum Viable Product (MVP) for the University of Canterbury's Enterprise Challenge, which is a start-up accelerator program. Through the program, the long-term vision for the site was to evolve into an online marketplace where farmers can trade commodities such as grains and livestock.",
